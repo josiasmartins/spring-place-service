232f9da32291fe541de80b2d5ed608ef3d57c4f8
josias.m.caitano <josiasmartinscaitano@gmail.com>
2023-11-19 09:58:43 -0300

    test: tested scenary success and error

--- src/test/java/br/com/josiasmartins/placeservice/PlaceServiceApplicationTests.java
+++ src/test/java/br/com/josiasmartins/placeservice/PlaceServiceApplicationTests.java
@@ -1,13 +1,54 @@
 package br.com.josiasmartins.placeservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
-class PlaceServiceApplicationTests {
+import br.com.josiasmartins.placeservice.api.PlaceRequest;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT) 
+class PlaceServiceApplicationTests { // teste de integração
+
+	@Autowired
+	WebTestClient webTestClient;
 
 	@Test
-	void contextLoads() {
+	public void testCreatePlaceSuccess() {
+		String name = "Valid Name";
+		String state = "Valid State";
+		String slug = "valid-name";
+		
+		webTestClient
+			.post()
+			.uri("/places")
+			.bodyValue(
+				new PlaceRequest(name, state)
+			)
+			.exchange() // efeatua a chamada para api 
+			.expectBody()
+				.jsonPath("name").isEqualTo(name)
+				.jsonPath("state").isEqualTo(state)
+				.jsonPath("slug").isEqualTo(slug)
+				.jsonPath("createdAt").isNotEmpty()
+				.jsonPath("updatedAt").isNotEmpty();
 	}
+
+	@Test
+	public void testCreatePlaceFailure() {
+		String name  = "";
+		String state = "";
+
+		webTestClient
+			.post()
+			.uri("/places")
+			.bodyValue(
+				new PlaceRequest(name, state)
+			)
+			.exchange() // efeatua a chamada para api 
+			.expectStatus().isBadRequest();
+	}	
 
 }
