d85a9ccb6f0012722ad58c4b611e8cf338dec47e
josias.m.caitano <josiasmartinscaitano@gmail.com>
2023-11-19 08:54:43 -0300

    feature: create schema and add @RequestBody in create PlaceController

--- src/main/java/br/com/josiasmartins/placeservice/web/PlaceController.java
+++ src/main/java/br/com/josiasmartins/placeservice/web/PlaceController.java
@@ -3,6 +3,7 @@ package br.com.josiasmartins.placeservice.web;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,9 +22,9 @@ public class PlaceController {
     }
 
     @PostMapping
-    public ResponseEntity<Mono<Place>> create(Place place) {
+    public ResponseEntity<Mono<Place>> create(@RequestBody Place place) {
         Mono<Place> createdPlace = placeService.create(place);
-        return ResponseEntity.status(HttpStatus.CREATED).body(null)
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdPlace);
     } 
     
 }
